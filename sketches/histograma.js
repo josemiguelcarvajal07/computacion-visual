b9b62e3ded96f471906bd4a863e772103dc6e48f
Acreed21 <cgcontrerasm@unal.edu.co>
2020-10-01 07:37:51 -0500

    Histograma

--- sketches/histograma.js
+++ sketches/histograma.js
@@ -1,8 +1,8 @@
 let img;
 let Bhist = true;
-let BhistR = false;
-let BhistG = false;
-let BhistB = false;
+let BhistR = true;
+let BhistG = true;
+let BhistB = true;
 let hist = [];
 let histR = [];
 let histG = [];
@@ -72,48 +72,42 @@ function draw() {
     noLoop();
 }
 
-function keyPressed() {
-    matrixsize = 3;
-    if (key == 'g') {
-        matrix = [[-1, -2, -1],
-        [0, 0, 0],
-        [1, 2, 1]];
-    } else if (key == 'h') {
-        matrix = [[1, 0, -1],
-        [2, 0, -2],
-        [1, 0, -1]];
-    } else if (key == 'i') {
-        matrix = [[1, 0, 1],
-        [-2, 0, 2],
-        [-1, 0, 1]];
-    } else if (key == 'j') {
-        matrix = [[1, 2, 1],
-        [0, 0, 0],
-        [-1, -2, -1]];
+function newImage(image) {
+    let hist = [256];
+    let histR = [256];
+    let histG = [256];
+    let histB = [256];
+  
+    for (var i = 0; i < image.width; i++) {
+      for (var j = 0; j < image.height; j++) {
+        var pixel = image.get(i, j);
+        hist[int(brightness(pixel))]++;
+        histR[int(red(pixel))]++;
+        histG[int(green(pixel))]++;
+        histB[int(blue(pixel))]++;
+      }
     }
-}
+    histMax = max(hist);
+    histRMax = max(histR);
+    histGMax = max(histG);
+    histBMax = max(histB);
+  }
 
-function convolution(x, y, k1, matrixsize, mask) {
-    var rtotal = 0.0;
-    var gtotal = 0.0;
-    var btotal = 0.0;
-    for (var i = 0; i < matrixsize; i++) {
-        for (var j = 0; j < matrixsize; j++) {
-            var xloc = x + i;
-            var yloc = y + j;
-            var loc = xloc + mask.width * yloc;
-            loc = constrain(loc, 0, mask.pixels.length - 1);
-            rtotal += (red(mask.pixels[loc])) * k1[i][j];
-            gtotal += (green(mask.pixels[loc])) * k1[i][j];
-            btotal += (blue(mask.pixels[loc])) * k1[i][j];
-        }
-    }
-    rtotal = constrain(rtotal, 0, 256);
-    gtotal = constrain(gtotal, 0, 256);
-    btotal = constrain(btotal, 0, 256);
-    if (!inv) {
-        return color(rtotal, gtotal, btotal);
-    } else {
-        return color(255 - rtotal, 255 - gtotal, 255 - btotal)
-    }
+function keyPressed() {
+    if(key == 'a'){
+        img = loadImage("Australia.jpg");
+        newImage(img);
+      }if(key == 'b'){
+        img = loadImage("RolandGarros.jpg");
+        newImage(img);
+      }if(key == 'c'){
+        img = loadImage("Wimbledon.jpg");
+        newImage(img);
+      }if(key == 'd'){
+        img = loadImage("USOpen.jpg");
+        newImage(img);
+      }if(key == 'e'){
+        img = loadImage("OldWimbledon.jpg");
+        newImage(img);
+      }
 }
