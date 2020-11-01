df52643463752d93a39de63d2ed2eec0a9ea05d3
Acreed21 <cgcontrerasm@unal.edu.co>
2020-10-31 20:34:11 -0500

    Cornsweet ilusion

--- sketches/cornsweet.js
+++ sketches/cornsweet.js
@@ -8,6 +8,10 @@ function draw() {
   noStroke();
   fill(172,172,172);
   rect(0, 0, 800, 400);
-  fill(59, 125, 191, k);
-  rect(380, 0, 80, 400);
+  for (var i = 370; i < 430; i++) {
+    fill(180,180,180);
+    rect(i, 0, i, 400);
+  }
+  fill(0, 0, 0, k);
+  rect(360, 0, 80, 400);
 }
