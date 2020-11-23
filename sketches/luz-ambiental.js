e08c7a109063e78b5aa17bf17c164b6416bfa238
Acreed21 <cgcontrerasm@unal.edu.co>
2020-11-23 12:45:34 -0500

    Ambiental js final

--- sketches/luz-ambiental.js
+++ sketches/luz-ambiental.js
@@ -1,19 +1,19 @@
 function setup() {
-  var myCanvas = createCanvas(400, 400, WEBGL);
+  var myCanvas = createCanvas(800, 400, WEBGL);
   myCanvas.parent("ambiental");
-  noStroke();
 }
 
 function draw() {
   background(0);
   let locX = mouseX - width / 2;
   let locY = mouseY - height / 2;
-  translate(-25, 0, 0);
-  lightFalloff(1, 0, 0);
-  pointLight(250, 250, 250, locX, locY, 50);
-  sphere(20);
-  translate(50, 0, 0);
-  lightFalloff(0.9, 0.01, 0);
-  pointLight(250, 250, 250, locX, locY, 50);
-  sphere(20);
+  pointLight(40, 125, 240, locX, locY, 125);
+  noStroke();
+  sphere(100);
+  rotateX(frameCount*0.01);
+  rotateY(frameCount*0.01);
+  translate(200,200,0);
+  sphere(50);
+  translate(0,-200,0);
+  cone(40,70) 
 }
