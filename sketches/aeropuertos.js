6ac92f788dd6bcf0282cd5c0ae54868883cf9752
josemiguelcarvajal07 <62575167+josemiguelcarvajal07@users.noreply.github.com>
2020-12-07 22:27:19 -0500

    sketch de aeropuertos

--- sketches/aeropuertos.js
+++ sketches/aeropuertos.js
@@ -1,91 +1,117 @@
 var table;
 var flights = [];
 
-var pg
+var pg;
 
-var flight = function(distance,from_long,from_lat,to_long,to_lat,from_country,to_country) {
-  this.distance = distance
-  this.from_long = from_long
-  this.from_lat = from_lat
-  this.to_long = to_long
-  this.to_lat = to_lat
-  this.from_country = from_country
-  this.to_country = to_country
+var flight = function (
+  from_airport,
+  from_city,
+  from_country,
+  from_long,
+  from_lat,
+  to_long,
+  to_lat) {
+  this.from_airport = from_airport;
+  this.from_city = from_city;
+  this.from_country = from_country;
+  this.from_long = from_long;
+  this.from_lat = from_lat;
+  this.to_long = to_long;
+  this.to_lat = to_lat;
 
-  this.departureX = map(this.from_long, -180,180,0,width)
-  this.departureY = map(this.from_lat, -90,90,height,0)  
-  this.arrivalX = map(this.to_long, -180,180,0,width)
-  this.arrivalY = map(this.to_lat, -90,90,height,0)
+  this.departureX = map(this.from_long, -180, 180, 0, width);
+  this.departureY = map(this.from_lat, -90, 90, height, 0);
+  this.arrivalX = map(this.to_long, -180, 180, 0, width);
+  this.arrivalY = map(this.to_lat, -90, 90, height, 0);
 
-  this.selected = function() {
-    if ( dist(mouseX, mouseY, this.departureX, this.departureY) < 5 ) {
-        return true
+  this.selected = function () {
+    if (dist(mouseX, mouseY, this.departureX, this.departureY) < 1.5) {
+      return true;
     } else {
-        return false
+      return false;
     }
-  }
+  };
 
-  this.drawBackgroundAirport = function() {
-    pg.ellipse(this.departureX, this.departureY, 5,5)
-    pg.ellipse(this.arrivalX, this.arrivalY, 5,5)
-  }
-	
-  this.drawSelectedAirport = function() {
-    if ( this.selected() ) {
-      fill(235, 56, 12,25)
-      ellipse(this.departureX, this.departureY, 5,5)
-      ellipse(this.arrivalX, this.arrivalY, 5,5)
+  this.drawBackgroundAirport = function () {
+    pg.ellipse(this.departureX, this.departureY, 5, 5);
+    pg.ellipse(this.arrivalX, this.arrivalY, 5, 5);
+  };
+
+  this.drawSelectedAirport = function () {
+    if (this.selected()) {
+      fill(235, 56, 12, 255);
+      ellipse(this.departureX, this.departureY, 5, 5);
+      ellipse(this.arrivalX, this.arrivalY, 5, 5);
     }
-  }
-}
+  };
+
+  this.printInfo = function () {
+    if (this.selected()) {
+      fill(255, 255, 255, 255);
+      textSize(30);
+      text(
+        `${this.from_airport}, ${this.from_city}, ${this.from_country}, (${this.from_lat},${this.from_long})`,
+        10,
+        700
+      );
+    }
+  };
+};
 
 function preload() {
-  table = loadTable("../taller-profundizacion/data/flights.csv","csv","header")
+  table = loadTable(
+    "../taller-profundizacion/data/flights.csv",
+    "csv",
+    "header"
+  );
 }
 
 function setup() {
-  var myCanvas = createCanvas(900, 500);
+  var myCanvas = createCanvas(1348, 774);
   myCanvas.parent("aeropuertos");
-  noStroke()
-  noLoop()
-	
-  var rows = table.getRows()
-  for ( var i in rows ) {
-    var from_airport = rows[i].getString("from_airport")
-    var from_city = rows[i].getString("from_city")
-    var from_country = rows[i].getString("from_country")
-    var from_long = rows[i].getNum("from_long")
-    var from_lat = rows[i].getNum("from_lat")
-    var to_airport = rows[i].getString("to_airport")
-    var to_city = rows[i].getString("to_city")
-    var to_country = rows[i].getString("to_country")
-    var to_long = rows[i].getNum("to_long")
-    var to_lat = rows[i].getNum("to_lat")
-    var airline = rows[i].getString("airline")
-    var airline_country = rows[i].getString("airline_country")
-    var distance = rows[i].getNum("distance")
+  noStroke();
+  noLoop();
 
-    var this_flight = new flight(distance, from_long, from_lat, to_long, to_lat, from_country, to_country)
-    flights.push(this_flight)
+  var rows = table.getRows();
+  for (var i in rows) {
+    var from_airport = rows[i].getString("from_airport");
+    var from_city = rows[i].getString("from_city");
+    var from_country = rows[i].getString("from_country");
+    var from_long = rows[i].getNum("from_long");
+    var from_lat = rows[i].getNum("from_lat");
+    var to_long = rows[i].getNum("to_long");
+    var to_lat = rows[i].getNum("to_lat");
+
+    var this_flight = new flight(
+      from_airport,
+      from_city,
+      from_country,
+      from_long,
+      from_lat,
+      to_long,
+      to_lat
+    );
+    flights.push(this_flight);
   }
 
-  pg = createGraphics(900,500);
+  pg = createGraphics(1348, 774);
   pg.background(100, 145, 217);
   pg.noStroke();
-  pg.fill(255,255,255,30);
-  for ( var i in flights ) {
-    flights[i].drawBackgroundAirport()
-	}
+  pg.fill(255, 255, 255, 30);
+  for (var i in flights) {
+    flights[i].drawBackgroundAirport();
+  }
 }
 
 function draw() {
-  background(100, 145, 217)
-  image(pg,0,0)
-  for ( var i in flights ) {
-    flights[i].drawSelectedAirport()
+  background(100, 145, 217);
+  image(pg, 0, 0);
+  for (var i in flights) {
+    flights[i].drawSelectedAirport();
+    flights[i].printInfo();
   }
 }
 
 function mouseMoved() {
-  redraw()
+  redraw();
 }
--- taller-profundizacion/aeropuertos.md
+++ taller-profundizacion/aeropuertos.md
@@ -1,12 +1,23 @@
 ---
-layout: sample
+layout: map
 title: Aeropuertos del mundo
 permalink: /aeropuertos/
 custom_css: style.css
 custom_js:
   - aeropuertos.js
 ---
+<p class="map-text">
+En esta pagina se puede observar un canvas en el cual se puede observar una representacion de todos los diferentes aeropuertos que hay en el mundo y las conexiones que existen entre ellos. Para realizar esta representacion se utilizo un archivo .csv en el cual se encontro variada informacion sobre cada aeropuerto en el mundo, cosas como nombre del aeropuerto, su ubicacion (latitud y longitud), la ciudad y el pais donde el aeropuerto se encuentra ubicado, y los diferentes aeropuertos con los que esta conectado.</p>
 
-adasdadasda
+<p class="map-text">
+Estas conexiones estan establecidas mediante los vuelos que las aerolineas ofrecen entre cada aeropuerto. La manera en la que este canvas funciona es mediante el uso del raton, cada punto blanco en el canvas representa un aeropuerto, cuando se hace hover sobre cualquiera de los puntos con el cursor, este cambia de color a naranja y tambien lo hacen todos los aeropuertos que esten relacionados con este, y finalmente en la parte inferior izquierda se muestra la siguiente información sobre este aeropuerto:</p>
 
-<div id='aeropuertos'></div>
+<ul class="map-text">
+    <li>Nombre del aeropuerto</li>
+    <li>Ciudad en la que se ubica el aeropuerto</li>
+    <li>Pais en el que se ubica el aeropuerto</li>
+    <li>Ubicación del aeropuerto en (Latitud, Longitud)</li>
+</ul>
+
+
+<div class="sketch-map" id='aeropuertos'></div>
